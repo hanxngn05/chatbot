9e2aa6140d92bdda7af25647100ae9e5cd7fe710
hanxngn05 <hn103@wellesley.edu>
2024-09-23 21:14:02 -0400

    cleaning files

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import json
 from difflib import get_close_matches
 
+# a text-based chatbot
 
 def load_knowledge_base(file_path: str) -> dict:
     with open(file_path, 'r') as file:
         data: dict = json.load(file)
         return data
 def save_knowledge_base(file_path: str, data: dict):
-    with open(file_path, 'w') as file:
+    with open(file_path, 'w') as file:c
         json.dump(data, file, indent=2)
 def find_best_match(user_question: str, questions: list[str]) -> str | None:
     matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
@@ -19,12 +20,12 @@ def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
 def chat_bot():
     knowledge_base: dict = load_knowledge_base('knowledge_base.json')
 
-    while True: 
+    while True:
         user_input: str = input('You: ')
         if user_input.lower() == "quit":
             break
         best_match: str | None = find_best_match(user_input, [q['question'] for q in knowledge_base['questions']])
-        
+
         if best_match:
             answer: str = get_answer_for_question(best_match, knowledge_base)
             print(f'Bot: {answer}')
@@ -35,6 +36,6 @@ def chat_bot():
             if new_answer.lower() != 'skip':
                 knowledge_base['questions'].append({'question': user_input, 'answer': new_answer})
                 save_knowledge_base('knowledge_base.json', knowledge_base)
-                print('Bot: Thank you! I learnt a new response!')     
+                print('Bot: Thank you! I learnt a new response!')
 if __name__ == '__main__':
     chat_bot()
